50dd078cd417dcbb55676cecaafc44d067ad3db3
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-11-26 18:49:19 +0530

    added db connection

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,36 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
+
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    useCreateIndex :true,
+    useNewUrlParser :true,
+    UseUnifiedTopology :true,
+    useFindAndModify :false
+
+
+}
+
+);
+
+const connection = mongoose.connection;
+connection.once("open",()=>
+{
+    console.log("mongoDB connection successful");
+}
+
+)
+
+app.listen(PORT,()=>
+
+{
+    console.log("server is up and runnging on port ${PORT} ");
+})
